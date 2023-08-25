c7b0df345dc8e9871b310f45c6fbb0cba299d784
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-08-24 21:15:40 -0400

    add check context in reload func

--- reload.go
+++ reload.go
@@ -7,11 +7,12 @@ import (
 )
 
 func (b *Browser) Reload() error {
-	// fmt.Println("Recargando Navegador")
-	err := chromedp.Run(b.Context, chromedp.Reload())
-	if err != nil {
-		return fmt.Errorf("error al recargar Pagina %v", err)
+	if b.Context != nil {
+		// fmt.Println("Recargando Navegador")
+		err := chromedp.Run(b.Context, chromedp.Reload())
+		if err != nil {
+			return fmt.Errorf("error al recargar Pagina %v", err)
+		}
 	}
-
 	return nil
 }
